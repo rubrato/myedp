09fe6db2e914f942d709fa4bc56c13a3ba0e9c5e
rui bragança <rbb_neto@hotmail.com>
2018-11-21 20:16:41 -0200

    adicionado um marcador temporário para a area selecionada, e criada uma popup com as informações do incident relatado no marcador

--- WebApplication1/Hubs/MarcadorHub.cs
+++ WebApplication1/Hubs/MarcadorHub.cs
@@ -12,52 +12,12 @@ namespace WebApplication1.Hubs
 			await Clients.All.SendAsync("getMarcador", lat, lng);
 		}
 
-		public async Task SendMarcador(double lat, double lng, string obs)
+		public async Task SendMarcador(double lat, double lng,string cpf, string nome, string telefone, string obs)
 		{
-			await Clients.All.SendAsync("sendMarcador", lat, lng, obs);
-
-		
-			//Demo: Basic ODP.NET Core application for ASP.NET Core
-			// to connect, query, and return results to a web page
-
-			//Create a connection to Oracle			
-			string conString = "User Id=system;Password=@Dmin123;" +
-
-			//How to connect to an Oracle DB without SQL*Net configuration file
-			//  also known as tnsnames.ora.
-			"Data Source=rubrato.myddns.me:1521/pi;";
-
-			//How to connect to an Oracle DB with a DB alias.
-			//Uncomment below and comment above.
-			//"Data Source=<service name alias>;";
-
-			using (OracleConnection con = new OracleConnection(conString))
-			{
-				using (OracleCommand cmd = con.CreateCommand())
-				{
-					
-					con.Open();
-					cmd.BindByName = true;
-
-					//Use the command to display employee names from 
-					// the EMPLOYEES table
-					cmd.CommandText = "select first_name from employees";
-
-
-
-					//Execute the command and use DataReader to display the data
-					OracleDataReader reader = cmd.ExecuteReader();
-					while (reader.Read())
-					{
-						string consulta = "reader.GetString(0)";
-						await Clients.All.SendAsync("sendConsulta", consulta);
-					}
-
-					reader.Dispose();
-				}
-			}
-
+			string pop = nome + " -   " + telefone + "<br> " + obs;
+			await Clients.All.SendAsync("sendMarcador", lat, lng, pop);
 			
+					
 		}
 	}
 }
--- WebApplication1/wwwroot/js/index.js
+++ WebApplication1/wwwroot/js/index.js
@@ -2,6 +2,38 @@
 
 mymap.locate({ setView: true, maxZoom: 16 });
 
+var selectmark = L.icon({
+    iconUrl: 'images/pinmark.png',
+
+    iconSize: [20, 20], // size of the icon
+    iconAnchor: [10, 20], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+var prb1mark = L.icon({
+    iconUrl: 'images/problm1.pg',
+
+    iconSize: [20, 20], // size of the icon
+    iconAnchor: [10, 20], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+var prb2mark = L.icon({
+    iconUrl: 'images/problm2.jpg',
+
+    iconSize: [20, 20], // size of the icon
+    iconAnchor: [10, 20], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+var prb3mark = L.icon({
+    iconUrl: 'images/problm3.jpg',
+
+    iconSize: [20, 20], // size of the icon
+    iconAnchor: [10, 20], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
@@ -9,8 +41,6 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     id: 'mapbox.streets'
 }).addTo(mymap);
-
-L.marker([-20.308811, -40.321348]).addTo(mymap);
 
 const connection = new signalR.HubConnectionBuilder()
     .withUrl("/marcador")
@@ -30,19 +60,29 @@ connection.on("sendMarcador", (lttd, lngtd, obs) => {
 connection.on("sendConsulta", (consulta) => {
     console.log(consulta);
 });
+$(document).ready(function () {
+    $(document).on('change', '#tipoproblema', function () {
+        console.log($(this).val());
 
+    });
+});
 
 var popup = L.popup();
 var sndlttd;
 var sndlngtd;
+var theMarker = {};
 
 function onMapClick(e) {
     //popup
     //    .setLatLng(e.latlng)
-    //    .setContent("You clicked the map at " + e.latlng.lat + ", " + e.latlng.lng)
-     //   .openOn(mymap);
+    //    .setContent("You clicked <br> the map at " + e.latlng.lat + ", " + e.latlng.lng)
+    //    .openOn(mymap);
     sndlttd = e.latlng.lat;
     sndlngtd = e.latlng.lng;
+    if (theMarker != undefined) {
+        mymap.removeLayer(theMarker);
+    };
+    theMarker = L.marker([sndlttd, sndlngtd], { icon: selectmark }).addTo(mymap)
     //connection.invoke('GetMarcador', e.latlng.lat, e.latlng.lng);
 }
 mymap.on('click', onMapClick);
@@ -52,8 +92,12 @@ var botaoAdicionar = document.querySelector("#adicionar-servico")
 
 botaoAdicionar.addEventListener("click", function () {
     event.preventDefault(); //evita a pagina ser recarregada ao clicar no botao
+    mymap.removeLayer(theMarker);
     var form = document.querySelector("#form-adicionar");
+    var cpf = form.cpf.value;
+    var nome = form.nome.value;
+    var telefone = form.telefone.value;
     var observacoes = form.observacoes.value;
-    console.log(observacoes);
-    connection.invoke('SendMarcador', sndlttd, sndlngtd, observacoes);
+    console.log(cpf + "<br>" + nome + "<br>" + telefone + "<br>" + observacoes);
+    connection.invoke('SendMarcador', sndlttd, sndlngtd, cpf, nome, telefone, observacoes);
 });
